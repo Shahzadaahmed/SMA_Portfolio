4e7e50f6a482d52c5bec33543fe0db5ba74b501e
Shahzada Muhammad Ahmed <shahzada.ahmed1996@gmail.com>
2026-08-11 23:09:47 +0500

    Favicon added and set

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" type="image/png" href="/icon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#0ea5e9" />
